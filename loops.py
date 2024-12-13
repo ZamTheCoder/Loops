2b461ab71e7afc23805a217421031fe05dbc862d
ZamTheCoder <rudhav935@gmail.com>
2024-12-13 18:46:20 +0530

    first commit

---
+++ loops.py
@@ -0,0 +1,5 @@
+num = int(input("Enter your Number: "))
+
+for i in range(1, 10+1):
+    print(num," x ", i, "=",num*i)
+56
---
+++ sum.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Your number: "))
+
+sum = 0
+for i in range(0, num+1):
+    sum = sum + i
+
+print("The sum is", sum)
